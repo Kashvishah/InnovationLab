251812fad46e58faca6c335abc200cfdd782d2b3
Kashvishah <42176318+Kashvishah@users.noreply.github.com>
2018-08-23 18:30:59 +0530

    Create addition.c

---
+++ addition.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int num1,num2,sum=0;
+    printf(" enter the number num1 and num2 :");
+    scanf("%d%d", &num1, &num2);
+    sum=num1+num2;
+    printf(" the addition of two numbers are %d",sum);
+}
